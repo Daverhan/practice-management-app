4d1f07b3fe2522df64fd21903939dba84f9fb5fe
David Silva <daverhan02@gmail.com>
2023-05-31 16:35:43 -0400

    integrated MAUI with test code

---
+++ PM.MAUI/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using PM.MAUI.ViewModels;
+
+namespace PM.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+                
+        }
+    }
+}
---
+++ PM.MAUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace PM.MAUI.ViewModels
+{
+    public class MainViewModel
+    {
+        public string Test1 {
+            get 
+            {
+                return "Test1";   
+            } 
+        }
+        public string Test2
+        {
+            get
+            {
+                return "Test2";
+            }
+        }
+        public string Test3
+        {
+            get
+            {
+                return "Test3";
+            }
+        }
+        public string Test4
+        {
+            get
+            {
+                return "Test4";
+            }
+        }
+    }
+}

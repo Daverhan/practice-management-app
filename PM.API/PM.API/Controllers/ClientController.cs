39173f36d9944c6ecdfc4e4004d120a68bb7508b
David Silva <daverhan02@gmail.com>
2023-06-26 15:09:44 -0400

    created WebAPI

---
+++ PM.API/PM.API/Controllers/ClientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.API.Database;
+using PM.Library.Models;
+
+namespace PM.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Client> Get()
+        {
+            return FakeDatabase.Clients;
+        }
+    }
+}
---
+++ PM.API/PM.API/Database/FakeDatabase.cs
@@ -0,0 +1,16 @@
+﻿using PM.Library.Models;
+
+namespace PM.API.Database
+{
+    public static class FakeDatabase
+    {
+        public static List<Client> Clients = new List<Client>
+        {
+            new Client {Id = 1, Name = "Client 1", IsActive = true },
+            new Client {Id = 2, Name = "Client 2", IsActive = true },
+            new Client {Id = 3, Name = "Client 3", IsActive = true },
+            new Client {Id = 4, Name = "Client 4", IsActive = true },
+            new Client {Id = 5, Name = "Client 5", IsActive = true }
+        };
+    }
+}

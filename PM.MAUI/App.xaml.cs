64edd2f7ea3b3f4980471d55bd5c9914ffc87db1
David Silva <daverhan02@gmail.com>
2023-07-18 20:41:47 -0400

    set minimum window size for desktop application

--- PM.MAUI/App.xaml.cs
+++ PM.MAUI/App.xaml.cs
@@ -5,8 +5,17 @@
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.MinimumWidth = 1440;
+            window.MinimumHeight = 810;
+
+            return window;
         }
     }
 }
